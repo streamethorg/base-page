f6a3e7196ef248c2a134f7fbd5ebc331863ce2e6
xvoorvaa <xander@xvproductions.com>
2024-04-29 13:04:36 +0200

    Added Not Found and updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,64 @@
+const shouldAnalyzeBundles = process.env.ANALYZE === true
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+let nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.FLUENTFFMPEG_COV': false,
+      })
+    )
 
-export default nextConfig;
+    return config
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  staticPageGenerationTimeout: 1000,
+}
+
+if (shouldAnalyzeBundles) {
+  console.log('Analyzing bundles..')
+  const withNextBundleAnalyzer = require('next-bundle-analyzer')({
+    enabled: true,
+  })
+  nextConfig = withNextBundleAnalyzer(nextConfig)
+}
+
+module.exports = nextConfig
