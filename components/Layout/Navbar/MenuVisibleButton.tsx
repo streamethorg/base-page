5189963012953344327a4620b0e8642cf21559aa
xvoorvaa <xander@xvproductions.com>
2024-05-03 11:56:20 +0200

    Fixed build

---
+++ components/Layout/Navbar/MenuVisibleButton.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import useSearchParams from '@/lib/hooks/useSearchParams'
+import { eTab } from '@/lib/types'
+import { Menu, X } from 'lucide-react'
+
+const MenuVisibleButton = () => {
+  const { searchParams, handleTermChange } = useSearchParams()
+  const menuVisible = searchParams.get('tab') !== eTab.none
+
+  const toggleMenu = () => {
+    if (!menuVisible) {
+      handleTermChange([{ key: 'tab', value: 'home' }])
+      return
+    }
+
+    handleTermChange([{ key: 'tab', value: 'none' }])
+  }
+
+  return (
+    <button onClick={toggleMenu} className="z-50">
+      {!menuVisible ? (
+        <Menu
+          size={30}
+          strokeWidth={2}
+          className="text-white text-muted-foreground"
+        />
+      ) : (
+        <X size={30} strokeWidth={1} className="text-white" />
+      )}
+    </button>
+  )
+}
+
+export default MenuVisibleButton
