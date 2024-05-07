5678ad68d24f2d13f555bc3bea3266e6ebf54a7a
xvoorvaa <xander@xvproductions.com>
2024-05-07 12:40:01 +0200

    Update VideoCardWithMenu.tsx

--- components/misc/VideoCard/VideoCardWithMenu.tsx
+++ components/misc/VideoCard/VideoCardWithMenu.tsx
@@ -39,11 +39,11 @@ const VideoCardWithMenu = async ({
       className="flex relative flex-col w-full min-h-full uppercase rounded-xl">
       <Thumbnail imageUrl={session.coverImage} />
 
-      <div className="flex absolute z-10 justify-between items-start w-full h-full bg-black bg-opacity-50">
-        <div
-          className={`rounded p-1 mt-1 lg:p-2 shadow-none lg:shadow-none `}>
-          <p
-            className={`px-2 uppercase line-clamp-2 overflow-hidden text-white font-bold  hover:underline text-xl sm:text-2xl md:text-lg xl:text-xl`}>
+      <div className="flex absolute justify-between items-start w-full h-full bg-black bg-opacity-50">
+        <div className="absolute inset-0 bg-black bg-opacity-50 transition-all duration-300 hover:backdrop-blur-sm"></div>
+
+        <div className="relative z-20 p-1 mt-1 lg:p-2">
+          <p className="overflow-hidden px-2 text-xl font-bold text-white uppercase sm:text-2xl md:text-lg xl:text-xl line-clamp-2">
             {session?.name}
           </p>
           <p className="flex items-center text-white">
@@ -54,14 +54,14 @@ const VideoCardWithMenu = async ({
             )}
           </p>
         </div>
-      </div>
 
-      <div className="flex absolute z-10 justify-start items-end p-4 w-full h-full transition-all md:opacity-0 hover:opacity-100 visibility-hidden hover:visibility-visible hover:backdrop-blur-sm">
-        <Button
-          className="transition-all hover:text-black hover:bg-gray-300"
-          variant="primary">
-          WATCH NOW
-        </Button>
+        <div className="flex absolute inset-0 justify-start items-end p-4 transition-opacity duration-300 md:opacity-0 hover:opacity-100">
+          <Button
+            className="transition-colors hover:text-black hover:bg-gray-300"
+            variant="primary">
+            WATCH NOW
+          </Button>
+        </div>
       </div>
 
       {DropdownMenuItems && (
