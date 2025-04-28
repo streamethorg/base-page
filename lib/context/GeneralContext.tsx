2f84db57040bde3e424afcdd0b81b8e045db7253
Pablo Voorvaart <pablo@streameth.org>
2025-04-28 12:47:14 +0100

    Add loading state to GeneralContext for SDK readiness

--- lib/context/GeneralContext.tsx
+++ lib/context/GeneralContext.tsx
@@ -3,13 +3,21 @@
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
 import SiweContext from './SiweContext'
-
+import { sdk } from '@farcaster/frame-sdk'
+import { useEffect, useState } from 'react'
 const GeneralContext = ({ children }: { children: any }) => {
+
+  const [isReady, setIsReady] = useState(false)
+  useEffect(() => {
+    sdk.actions.ready().then(() => {
+      setIsReady(true)
+    })
+  }, [])
   return (
     <SiweContext>
       <Analytics />
       <SpeedInsights />
-      {children}
+      {isReady ? children : <div>Loading...</div>}
     </SiweContext>
   )
 }
